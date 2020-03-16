57af39cbeaea826f7937325b1a39ba7645690758
Vicki <vicki.kalm@gmail.com>
2020-03-16 22:46:36 +0200

    refactor code

--- client/src/components/search/search.js
+++ client/src/components/search/search.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import './search.css';
 
-//import 'https://www.gettyimages.com/detail/photo/paint-explosion-royalty-free-image/170955250';
-//value={this.props.search}
 class Search extends React.Component {
     render() {
         return (
